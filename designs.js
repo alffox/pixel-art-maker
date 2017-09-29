9e5af22bdb216b7bc192ec38ae08198127240ef0
alffox <alfonso.crisci.it@gmail.com>
2017-09-29 12:16:56 +0200

    starting files with functions to build grid based on user inputs

---
+++ designs.js
@@ -0,0 +1,29 @@
+// Select color input
+// Select size input
+
+
+
+
+
+$("#sizePicker").submit(function(event) {
+    var gridHeight = $("#input_height").val();
+    var gridWidth = $("#input_width").val();
+    $("#input").next().text();
+    //prevent refreshing upon submission
+    event.preventDefault();
+
+    // When size is submitted by the user, call makeGrid()
+    makeGrid();
+
+    function makeGrid() {
+        //first, let's clear any pre-existing grid rendered on page as a result of any previous submissions
+        $("#pixel_canvas").find("tr","td").remove();
+        //build actual grid
+        for (var row = 0; row < gridWidth; row++) {
+            $("#pixel_canvas").append("<tr></tr>");
+            for (var column = 0; column < gridHeight; column++) {
+                $("tr").last().append("<td></td>");
+            }
+        }
+    }
+});
