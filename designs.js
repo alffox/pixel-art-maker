6811f4d26a7a84a3dc85525ca3193c54f5c771af
Alfonso <alfonso.crisci.it@gmail.com>
2017-10-05 20:53:05 +0200

    meets projects requirements

--- designs.js
+++ designs.js
@@ -1,4 +1,4 @@
-    //declaring isMouseDown boolean to use when keeping the mouse button pressed
+    // declaring isMouseDown boolean to use when keeping the mouse button pressed
     var isMouseDown;
 
     $("body").mousedown(function(){
@@ -9,35 +9,37 @@
         isMouseDown = false;
     });
 
-    //get user inputs to build the grid
+    // get user inputs to build the grid
     $("#sizePicker").submit(function(event) {
         var gridHeight = $("#input_height").val();
         var gridWidth = $("#input_width").val();
         $("#input").next().text();
-    //prevent grid refreshing upon submission
+    //prevent grid from refreshing upon submission
     event.preventDefault();
     // When size is submitted by the user, build actual grid
     makeGrid();
 
     function makeGrid() {
-        //first, let's clear any pre-existing grid rendered on page as a result of any previous submissions
+        // clear any previous grids
         $("#pixel_canvas").find("tr","td").remove();
-        //build actual grid HTML elements
+        // build actual grid HTML elements
         for (var row = 0; row < gridWidth; row++) {
             $("#pixel_canvas").append("<tr></tr>");
             for (var column = 0; column < gridHeight; column++) {
                 $("tr").last().append("<td></td>");
             }
         }
-        //as long as the mouse is pressed, color table cells
+
+        // as long as the mouse is pressed, color table cells
         $("td").mouseover(function() {
             if (isMouseDown)
                 var chosenColor = $("#colorPicker").val();
             $(this).css("backgroundColor", chosenColor);
-        });
-        $("td").click(function() {
-            var chosenColor = $("#colorPicker").val();
-            $(this).css("backgroundColor", chosenColor);
+            // even a single mouse click should color a cell
+            $("td").click(function() {
+                var chosenColor = $("#colorPicker").val();
+                $(this).css("backgroundColor", chosenColor);
+            });
         });
     };
 });
